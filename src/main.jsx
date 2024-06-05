26f42a24443ed5f8caa9ac28bd787bf2f09b0a50
JAYDIP <pateljaydippj699@gmail.com>
2024-06-05 16:42:27 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import InputBox from './components/InputBox.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+    <InputBox></InputBox>
+  </React.StrictMode>,
+)
+
